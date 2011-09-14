6aa39a029ef54aa06fb253a824d321dbb1f3b812
evanzhou2000@gmail.com <evanzhou2000@gmail.com@ad8c41d9-ee36-816e-b0c8-139c1fce2b43>
2011-09-14 01:20:33 +0000

    Finish use Flex

---
+++ trunk/iVelocity/LexLayer.h
@@ -0,0 +1,11 @@
+/*
+ *  LexLayer.h
+ *  iVelocity
+ *
+ *  Created by chunyi.zhoucy on 11-9-10.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+extern int yylex(int *yylval);
+extern void initLex(NSString *strTemplate);
